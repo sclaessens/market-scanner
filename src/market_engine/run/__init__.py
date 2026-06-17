e7f9b717f332203bbaa68c227693cddf371a8cc7
sclaessens <stevenclaessens@gmail.com>
2026-06-17 11:01:03 +0200

    ME-RUN02 export dry-run harness

---
+++ src/market_engine/run/__init__.py
@@ -0,0 +1,23 @@
+from market_engine.run.end_to_end_dry_run import (
+    APPROVED_DRY_RUN_INPUT_MODES,
+    MARKET_ENGINE_END_TO_END_DRY_RUN_BOUNDARY,
+    MARKET_ENGINE_END_TO_END_DRY_RUN_FORMAT_VERSION,
+    REQUIRED_DRY_RUN_STAGE_NAMES,
+    MarketEngineEndToEndDryRun,
+    MarketEngineEndToEndDryRunStageResult,
+    MarketEngineEndToEndDryRunStageStatus,
+    MarketEngineEndToEndDryRunState,
+    build_market_engine_end_to_end_dry_run,
+)
+
+__all__ = [
+    "APPROVED_DRY_RUN_INPUT_MODES",
+    "MARKET_ENGINE_END_TO_END_DRY_RUN_BOUNDARY",
+    "MARKET_ENGINE_END_TO_END_DRY_RUN_FORMAT_VERSION",
+    "REQUIRED_DRY_RUN_STAGE_NAMES",
+    "MarketEngineEndToEndDryRun",
+    "MarketEngineEndToEndDryRunStageResult",
+    "MarketEngineEndToEndDryRunStageStatus",
+    "MarketEngineEndToEndDryRunState",
+    "build_market_engine_end_to_end_dry_run",
+]
